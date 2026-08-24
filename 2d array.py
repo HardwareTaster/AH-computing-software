1b8392ebbfeee58ff428e1af7f2e3920cc5d143a
HardwareTaster <a.artikov@albynschool.org>
2026-08-24 09:35:28 +0100

    Update 2d array.py

--- 2d array.py
+++ 2d array.py
@@ -19,3 +19,39 @@ for row in range (rows):
 			counter+= 1
 
 print (counter)
+
+
+#-------------------------------------------------------------------------------------------------
+
+
+
+board = [
+	['X', 'X', 'X'],
+	['O', 'X', 'O'],
+	['O', 'O', 'X']
+]
+ 
+for row in range(3):
+	print(board[row])
+ 
+winner = ''
+ 
+# TODO 1: check rows
+for row in range (3):
+ 	if board[row][0]== board[row][1] == board[row][2]:
+ 		winner = board[row][0]
+ 
+# TODO 2: check columns
+for col in range (3):
+	if board[0][col]== board[1][col] == board[2][col]:
+			winner = board[0][col]
+# TODO 3: check diagonals
+for row in range (3):
+	if board[0][0]== board[1][1] == board[2][2] or board[0][2]== board[1][1] == board[2][0]:
+		winner = board[1][1]
+# TODO 4: report the result
+
+if winner == '':
+	print('No winner')
+else:
+    print(winner, 'has won')
